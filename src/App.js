00657575366188c674a0bb8667eb83ceefb743cf
Jack-Heaton <43050713+Jack-Heaton@users.noreply.github.com>
2020-04-09 20:56:31 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import awsconfig from '../aws-exports'
+import awsconfig from './src/aws-exports'
 
 function App() {
   return (
