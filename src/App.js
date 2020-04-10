e4374c06584de789a62d514702b18ba3379548e8
Jack-Heaton <43050713+Jack-Heaton@users.noreply.github.com>
2020-04-09 21:48:03 -0400

    test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          {awsconfig.aws_cognito_region}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
